632795339b567e6d9deadcc0473326f5a30caf3c
unknown <rafaelheitor2@outlook.com>
2022-06-01 21:11:28 -0300

    Propriedades de formularios no React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+export default function App() {
+  const [formData, setFormData] = useState({firstName: "", lastName: "", email: "", employment: ""})
+
+console.log(formData.employment)
+  function handleChange(event){
+    const {name, value, type, checked} = event.target 
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <form>
+     <input 
+     type="text" placeholder="First Name"
+      name="firstName"
+      value={formData.firstName}
+      onChange={handleChange}
+      />
+
+     <input 
+     type="text" placeholder="Last Name"
+      name="lastName"
+      value={formData.lastName}
+      onChange={handleChange}
+      />
+
+     <input 
+     type="text" placeholder="Email" 
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+      />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input 
+          type="radio" 
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="unemployed">Unemployed</label>
+
+        <input 
+          type="radio" 
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="part-time">Part-time</label>
+
+        <input 
+          type="radio" 
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+       <label htmlFor="full-time">Full time</label>
+
+      </fieldset>
+
+   </form>
   );
 }
 
-export default App;
